99fd84b71af0c69cfa796c13e29783f0044a7758
Albina <kamalieva.albish@mail.ru>
2024-06-06 16:41:40 +0300

    Добавьте файлы проекта.

---
+++ HomeWork15/Program.cs
@@ -0,0 +1,29 @@
+﻿using HomeWork15;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var stud = classes.SelectMany(x => x.Students);
+            return stud.ToArray();    
+        }
+    }
+}
